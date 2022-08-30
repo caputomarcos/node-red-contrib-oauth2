5551f392737ca216dbba14fbf91e8c8bb8a1263f
caputomarcos <caputo.marcos@gmail.com>
2022-08-30 08:50:18 -0300

    OAuth2 Authorization Code grant support #27

--- oauth2.js
+++ oauth2.js
@@ -23,11 +23,27 @@
  *
  **/
 
-module.exports = function (RED) {
+ module.exports = function (RED) {
   "use strict";
   // require any external libraries we may need....
-  let request = require("request");
+  const request = require("request");
 
+  const crypto = require("crypto");
+  const url = require('url');
+
+  const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key, value) => {
+      if (typeof value === 'object' && value !== null) {
+        if (seen.has(value)) {
+          return;
+        }
+        seen.add(value);
+      }
+      return value;
+    };
+  };
+  
   // The main node definition - most things happen in here
   class OAuth2Node {
     constructor(oauth2Node) {
@@ -102,6 +118,9 @@ module.exports = function (RED) {
             options.form.username = node.username;
             options.form.password = node.password;
           };
+          if (node.grant_type === "authorization_code") {
+            options.form.code = node.credentials.code;
+          };
         };
 
         // add any custom headers, if we haven't already set them above
@@ -148,14 +167,83 @@ module.exports = function (RED) {
         });
       });
     }
-  }
+  };
 
-  RED.httpAdmin.get('/oauth/redirect', function(req, res) {
-    if (!req.query.clientId || !req.query.redirect_uri ||
-      !req.query.id || !req.query.callback) {
-      res.send(400);
-      return;
+  RED.nodes.registerType("oauth2-credentials",OAuth2Node,{
+    credentials: {
+        displayName: {type:"text"},
+        clientId: {type:"text"},
+        clientSecret: {type:"password"},
+        accessToken: {type:"password"},
+        refreshToken: {type:"password"},
+        expireTime: {type:"password"},
+        code: {type:"password"}
     }
+  });
+
+
+  RED.httpAdmin.get('/oauth2/credentials/:token', function(req, res) {
+    var credentials = RED.nodes.getCredentials(req.params.token);
+    res.json({code: credentials.code});
+  });
+
+  RED.httpAdmin.get('/oauth2/redirect', function(req, res) {
+    if (req.query.code) {
+      var state = req.query.state.split(':');
+      var node_id = state[0];
+      var credentials = RED.nodes.getCredentials(node_id);
+      credentials.code = req.query.code;
+      res.send('Authorized');
+    }
+  });
+
+  
+  RED.httpAdmin.get('/oauth2/auth', function(req, res){
+    console.log('oauth2/auth');
+    if (!req.query.clientId || !req.query.clientSecret ||
+        !req.query.id || !req.query.callback) {
+        res.send(400);
+        return;
+    }
+    const node_id = req.query.id;
+    const callback = req.query.callback;
+    const redirectUri = req.query.redirectUri;
+    const credentials = JSON.parse(JSON.stringify(req.query, getCircularReplacer()))
+    const scopes = req.query.scopes;
+    const csrfToken = crypto.randomBytes(18).toString('base64').replace(/\//g, '-').replace(/\+/g, '_');
+    credentials.csrfToken = csrfToken;
+    credentials.callback = callback;
+    res.cookie('csrf', csrfToken);
+    res.redirect(url.format({
+      protocol: 'https',
+      hostname: 'github.com',
+      pathname: '/login/oauth/authorize',
+      query: {
+          client_id: credentials.clientId,
+          redirect_uri: redirectUri,
+          state: node_id + ":" + csrfToken
+      }
+    }));
+    // res.redirect(`${req.query.authorizationEndpoint}?client_id=${req.query.clientId}&redirect_uri=https://flow.essavida.ai/red/oauth/redirect&id=${req.query.id}`);
+    RED.nodes.addCredentials(node_id, credentials);
+ });
+
+  RED.httpAdmin.get('/oauth2/auth/callback', function(req, res) {
+    console.log('oauth/auth/callback');
+    if (req.query.error) {
+        return res.send("oauth2.error.error", {error: req.query.error, description: req.query.error_description});
+    }
+    var state = req.query.state.split(':');
+    var node_id = state[0];
+    var credentials = RED.nodes.getCredentials(node_id);
+    if (!credentials || !credentials.clientId || !credentials.clientSecret) {
+        console.log("credentials not present?");
+        return res.send("oauth2.error.no-credentials");
+    }
+    if (state[1] !== credentials.csrfToken) {
+        return res.status(401).send("oauth2.error.token-mismatch");
+    }
+
   });
 
   RED.nodes.registerType("oauth2", OAuth2Node);
