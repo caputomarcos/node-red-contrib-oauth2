0406daa69fd8d81480d63356322cc81491589eca
caputo.marcos <caputo.marcos@gmail.com>
2023-05-20 00:23:57 -0300

    fix authetication code proxy

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,34 +1,38 @@
-// for node (client) and config (server)
+/**
+ * Creates a backward compatible object by assigning default values to properties if they are undefined.
+ *
+ * @param {Object} obj - The object to make backward compatible.
+ * @returns {void}
+ */
 const CreateBackwardCompatible = (obj) => {
-  if (typeof obj.name === 'undefined') obj.name = ''
-  if (typeof obj.container === 'undefined') obj.container = ''
-  if (typeof obj.containerOpts === 'undefined') obj.containerOpts = ''
-  if (typeof obj.errorHandling === 'undefined') obj.errorHandling = ''
-  
-  if (typeof obj.grantType === 'undefined') obj.grantType = ''
-  if (typeof obj.grantOpts === 'undefined') obj.grantOpts = ''
-  if (typeof obj.accessTokenUrl === 'undefined') obj.accessTokenUrl =  ''
-  if (typeof obj.clientSecret === 'undefined') obj.clientSecret =  ''
-  if (typeof obj.clientId === 'undefined') obj.clientId =  ''
-  if (typeof obj.scope === 'undefined') obj.scope =  ''
-  if (typeof obj.userName === 'undefined') obj.userName =  ''
-  if (typeof obj.password === 'undefined') obj.password =  ''
-  if (typeof obj.authorizationEndpoint === 'undefined') obj.authorizationEndpoint =  ''
-  if (typeof obj.code === 'undefined') obj.code =  ''
+  if (typeof obj.name === 'undefined') obj.name = '';
+  if (typeof obj.container === 'undefined') obj.container = '';
+  if (typeof obj.containerOpts === 'undefined') obj.containerOpts = '';
+  if (typeof obj.errorHandling === 'undefined') obj.errorHandling = '';
 
-  if (typeof obj.internalErrors === 'undefined') obj.internalErrors = {}
-  if (typeof obj.devMode === 'undefined') obj.devMode = false
-  if (typeof obj.keepAuth === 'undefined') obj.keepAuth = false 
-  if (typeof obj.rejectUnauthorized === 'undefined') obj.rejectUnauthorized = false
-  if (typeof obj.clientCredentialsInBody === 'undefined') obj.clientCredentialsInBody = false
-  if (typeof obj.clientCredentialsInBody === 'undefined') obj.clientCredentialsInBody = false 
-  if (typeof obj.headers === 'undefined') obj.headers = {} 
-  if (typeof obj.proxy === 'undefined') obj.proxy = '' 
+  if (typeof obj.grantType === 'undefined') obj.grantType = '';
+  if (typeof obj.grantOpts === 'undefined') obj.grantOpts = '';
+  if (typeof obj.accessTokenUrl === 'undefined') obj.accessTokenUrl = '';
+  if (typeof obj.clientSecret === 'undefined') obj.clientSecret = '';
+  if (typeof obj.clientId === 'undefined') obj.clientId = '';
+  if (typeof obj.scope === 'undefined') obj.scope = '';
+  if (typeof obj.userName === 'undefined') obj.userName = '';
+  if (typeof obj.password === 'undefined') obj.password = '';
+  if (typeof obj.authorizationEndpoint === 'undefined') obj.authorizationEndpoint = '';
+  if (typeof obj.code === 'undefined') obj.code = '';
 
-  if (typeof obj.showBanner === 'undefined') obj.showBanner = false
-  if (typeof obj.disableInput === 'undefined') obj.disableInput = false 
-}
+  if (typeof obj.internalErrors === 'undefined') obj.internalErrors = {};
+  if (typeof obj.devMode === 'undefined') obj.devMode = false;
+  if (typeof obj.keepAuth === 'undefined') obj.keepAuth = false;
+  if (typeof obj.rejectUnauthorized === 'undefined') obj.rejectUnauthorized = false;
+  if (typeof obj.clientCredentialsInBody === 'undefined') obj.clientCredentialsInBody = false;
+  if (typeof obj.headers === 'undefined') obj.headers = {};
+  if (typeof obj.proxy === 'undefined') obj.proxy = '';
+
+  if (typeof obj.showBanner === 'undefined') obj.showBanner = false;
+  if (typeof obj.disableInput === 'undefined') obj.disableInput = false;
+};
 
 module.exports = {
   CreateBackwardCompatible
-}
+};
--- src/oauth2.js
+++ src/oauth2.js
@@ -59,10 +59,7 @@ module.exports = function (RED) {
       };
 
       CreateBackwardCompatible(config);
-
       let payload = StoreCredentials(RED, config, msg);
-
-      // const payload = RED.nodes.getCredentials(config.id);
       getAccessToken(payload)
         .then((data) => {
           setTimeout(() => {
@@ -93,7 +90,7 @@ module.exports = function (RED) {
         redirectUri: credentials.redirectUri,
       });
     } else {
-      return res.status(400).send("oauth2.error.no-credentials");
+      return res.status(401).json({ error: "oauth2.error.no-credentials" });
     }
   });
 
@@ -110,26 +107,54 @@ module.exports = function (RED) {
         credentials.code = req.query.code ? req.query.code : null;
         credentials.state = req.query.state ? req.query.state : null;
         RED.nodes.addCredentials(node_id, credentials);
-        const html = `<HTML>
-      <HEAD>
-          <script language=\"javascript\" type=\"text/javascript\">
+        const html = `<html>
+          <head>
+          <script language="javascript" type="text/javascript">
             function closeWindow() {
-                window.open('','_parent','');
-                window.close();
+              window.open("", "_parent", "");
+              window.close();
             }
-            function delay() {\n
-                setTimeout(\"closeWindow()\", 1000);\n
-            }\n
+            function delay() {
+              setTimeout("closeWindow()", 2000);
+            }
           </script>
-      </HEAD>
-      <BODY onload=\"javascript:delay();\">
-              <p>Success! This page can be closed if it doesn't do so automatically.</p>
-      </BODY>
-      </HTML>`;
+        </head>
+        <body onload="javascript:delay();">
+            <div style="text-align: center;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200">
+                <circle cx="100" cy="100" r="90" fill="#42b983" />
+                <text x="50%" y="50%" text-anchor="middle" fill="#ffffff" font-size="24px" font-weight="bold" dy=".3em">Success!</text>
+              </svg>
+              <p>This page can be closed if it doesn't do so automatically.</p>
+            </div>
+          </body>
+        </html>`;
         return res.status(200).send(html);
       }
     } else {
-      return res.status(400).send("oauth2.error.no-credentials");
+      const html = `<html>
+       <head>
+          <script language="javascript" type="text/javascript">
+            function closeWindow() {
+              window.open("", "_parent", "");
+              window.close();
+            }
+            function delay() {
+              setTimeout("closeWindow()", 2000);
+            }
+          </script>
+        </head>
+        <body onload="javascript:delay();">        
+          <div style="text-align: center;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200">
+              <circle cx="100" cy="100" r="90" fill="#ff0000" />
+              <text x="50%" y="50%" text-anchor="middle" fill="#ffffff" font-size="24px" font-weight="bold" dy=".3em">Error 400</text>
+            </svg>
+            <p>An error occurred. Please check your credentials and try again.</p>
+          </div>
+        </body>
+      </html>`;
+      return res.status(400).send(html);
     }
   });
 
@@ -145,12 +170,10 @@ module.exports = function (RED) {
       !req.query.id ||
       !req.query.callback
     ) {
-      return res.status(400).send("Bad Request");
+      return res.status(400).json({ error: "Bad Request" });
     }
 
     const node_id = req.query.id;
-    const callback = req.query.callback;
-    const redirectUri = req.query.redirectUri;
     let credentials = JSON.parse(
       JSON.stringify(req.query, getCircularReplacer())
     );
@@ -165,13 +188,12 @@ module.exports = function (RED) {
           protocol: proxyURL.protocol,
           hostname: proxyURL.hostname,
           port: proxyURL.port,
-          username: proxyURL.username,
-          password: proxyURL.password,
+          username: proxy.credentials.username,
+          password: proxy.credentials.password,
         };
       }
     }
 
-    const scope = req.query.scope;
     const csrfToken = crypto
       .randomBytes(18)
       .toString("base64")
@@ -197,7 +219,31 @@ module.exports = function (RED) {
       });
       res.redirect(response.request.res.responseUrl);
     } catch (error) {
-      return res.sendStatus(error.response.status);
+      const statusCode = error.response.status;
+      const statusText = error.response.statusText;
+      const html = `<html>
+        <head>
+          <script language="javascript" type="text/javascript">
+            function closeWindow() {
+              window.open("", "_parent", "");
+              window.close();
+            }
+            function delay() {
+              setTimeout("closeWindow()", 2000);
+            }
+          </script>
+        </head>
+        <body onload="javascript:delay();">
+          <div style="text-align: center;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200">
+              <circle cx="100" cy="100" r="90" fill="#ff0000" />
+              <text x="50%" y="50%" text-anchor="middle" fill="#ffffff" font-size="24px" font-weight="bold" dy=".3em">Error ${statusCode}</text>
+            </svg>
+            <p>${statusText}</p>
+          </div>
+        </body>
+      </html>`;
+      return res.status(statusCode).send(html);
     }
   });
   /**
@@ -220,7 +266,7 @@ module.exports = function (RED) {
       return res.send("oauth2.error.no-credentials");
     }
     if (state[1] !== credentials.csrfToken) {
-      return res.status(401).send("oauth2.error.token-mismatch");
+      return res.status(401).json({ error: "oauth2.error.token-mismatch" });
     }
   });
 
